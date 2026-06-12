9383400ff04cd1292575e9da513012423aaf5f4d
Pat <patrycja.kruczynska@gmail.com>
2026-06-13 00:58:29 +0200

    change profile

--- app/components/ProfileSection.jsx
+++ app/components/ProfileSection.jsx
@@ -82,7 +82,7 @@ const ProfileSection = () => {
 
             <div className="relative rounded-full overflow-hidden border border-white/10 backdrop-blur-sm">
               <Image
-                src="/images/prof.jpg"
+                src="/images/prof.jpeg"
                 alt="Profile picture"
                 width={350}
                 height={350}
